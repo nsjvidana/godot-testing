9237bef35a9ab23d6aa658485748fde428be342a
AnonymousAcid <nilsonjvidana@gmail.com>
2024-11-12 21:21:09 -0800

    Fix custom y-billboarding
    it did billboarding on more than just the y axis. this commit fixes that issue.

--- Scenes/SpritePerspective.cs
+++ Scenes/SpritePerspective.cs
@@ -5,6 +5,8 @@ public partial class SpritePerspective : Sprite3D {
 
 	[Export]
 	public SpriteFrames perspectives;
+	[Export]
+	public Node3D player;
 
 	double dt;
 	int spriteIndex;
@@ -17,12 +19,40 @@ public partial class SpritePerspective : Sprite3D {
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta) {
-		dt += delta;
-		if(dt > 1.0) {
-			Texture = perspectives.GetFrameTexture("default", spriteIndex);
-			spriteIndex++;
-			spriteIndex %= frameCount;
-			dt = 0.0;
+		// dt += delta;
+		// if(dt > 1.0) {
+		// 	Texture = perspectives.GetFrameTexture("default", spriteIndex);
+		// 	spriteIndex++;
+		// 	spriteIndex %= frameCount;
+		// 	dt = 0.0;
+		// }
+
+		//billboarding
+		//not going to use Sprite3D.Billboard because setting the sprite perspective
+		//requires that  we know the rotation for billboarding.
+		var camPos = GetViewport().GetCamera3D().GlobalTransform.Origin;
+			camPos.Y = 0;
+		var currYPos = GlobalPosition.Y;
+		GlobalPosition = new Vector3(GlobalPosition.X, 0f, GlobalPosition.Z);//temporarily set y pos to 0 for LookAt calculation
+		LookAt(camPos, Vector3.Up);
+		GlobalPosition = new Vector3(GlobalPosition.X, currYPos, GlobalPosition.Z);
+
+		//0 - back
+		//1 - facing left
+		//2 - front
+		//3 - facing right
+		var rotDiff = Rotation.Y - player.Rotation.Y;
+		var rotDiffAbs = Mathf.Abs(rotDiff);
+		GD.Print($"{rotDiff}, {Mathf.Pi/2f}");
+		if(rotDiffAbs <= Mathf.Pi/4f) 
+			Texture = perspectives.GetFrameTexture("default", 2);
+		else if(Mathf.Abs(rotDiffAbs-Mathf.Pi/2f) <= Mathf.Pi/4f) {
+			if(Mathf.Sign(rotDiff) == 1)
+				Texture = perspectives.GetFrameTexture("default", 3);
+			else
+				Texture = perspectives.GetFrameTexture("default", 1);
 		}
+		else
+			Texture = perspectives.GetFrameTexture("default", 0);
 	}
 }
--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -4,8 +4,10 @@ using System.Diagnostics;
 
 public partial class Player : CharacterBody3D
 {
-	public const float Speed = 5.0f;
-	public const float JumpVelocity = 4.5f;
+	[Export]
+	public float Speed = 5.0f;
+	[Export]
+	public float JumpVelocity = 4.5f;
 
 	public override void _PhysicsProcess(double delta)
 	{
