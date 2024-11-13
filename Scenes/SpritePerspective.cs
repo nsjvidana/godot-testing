2941dbd6701788e556a40217fc04b9cb484bfd57
AnonymousAcid <nilsonjvidana@gmail.com>
2024-11-12 21:23:14 -0800

    Fix oversized player collider

--- Scenes/SpritePerspective.cs
+++ Scenes/SpritePerspective.cs
@@ -43,16 +43,19 @@ public partial class SpritePerspective : Sprite3D {
 		//3 - facing right
 		var rotDiff = Rotation.Y - player.Rotation.Y;
 		var rotDiffAbs = Mathf.Abs(rotDiff);
-		GD.Print($"{rotDiff}, {Mathf.Pi/2f}");
-		if(rotDiffAbs <= Mathf.Pi/4f) 
+		if(rotDiffAbs <= Mathf.Pi/8f) 
 			Texture = perspectives.GetFrameTexture("default", 2);
-		else if(Mathf.Abs(rotDiffAbs-Mathf.Pi/2f) <= Mathf.Pi/4f) {
-			if(Mathf.Sign(rotDiff) == 1)
-				Texture = perspectives.GetFrameTexture("default", 3);
-			else
-				Texture = perspectives.GetFrameTexture("default", 1);
+		else if(Mathf.Abs(rotDiffAbs-Mathf.Pi/8f) <= Mathf.Pi/8f) {
+			
 		}
-		else
-			Texture = perspectives.GetFrameTexture("default", 0);
+		
+		// else if(Mathf.Abs(rotDiffAbs-Mathf.Pi/2f) <= Mathf.Pi/4f) {
+		// 	if(Mathf.Sign(rotDiff) == 1)
+		// 		Texture = perspectives.GetFrameTexture("default", 3);
+		// 	else
+		// 		Texture = perspectives.GetFrameTexture("default", 1);
+		// }
+		// else
+		// 	Texture = perspectives.GetFrameTexture("default", 0);
 	}
 }
