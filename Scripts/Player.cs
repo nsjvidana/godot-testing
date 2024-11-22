3ff3c87de5e1c41e61e600b754095e56c1e90ec7
AnonymousAcid <nilsonjvidana@gmail.com>
2024-11-21 16:27:20 -0800

    use custom input map for movement

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -35,7 +35,7 @@ public partial class Player : CharacterBody3D
 
 		// Get the input direction and handle the movement/deceleration.
 		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 inputDir = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+		Vector2 inputDir = Input.GetVector("left", "right", "forward", "back");
 		Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
 			direction = direction.Rotated(Vector3.Up, springArm.Rotation.Y).Normalized();
 		if (direction != Vector3.Zero)
